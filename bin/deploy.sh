1ee285d4511716e293de365e00d7359317fd3483
Alex <misterksn@gmail.com>
2021-05-12 10:20:02 +0300

    Fix deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
+set -e
 
-unset GIT_INDEX_FILE
-
-export ROOT=/home/estateza
-export DOMAIN=redatabase
-export REPO=redatabase.git
-
-while read oldrev newrev ref
-do
-    if [[ $ref =~ .*/master$ ]]; then
-        export GIT_DIR="$ROOT/repos/$REPO/"
-        export GIT_WORK_TREE="$ROOT/domains/$DOMAIN/"
-        git checkout -f master
-        cd $GIT_WORK_TREE
-        ./deploy.sh
-    else
-        echo "Doing nothing: only the master branch can be deployed on this server."
-    fi
-done
-
-
-
-docker-compose up --remove-orphans
+docker-compose down
+docker image prune -f
+docker-compose up --remove-orphans -d
