0d7c72c6b032eebf71f08a6880f8feba80b3b03e
Alex <misterksn@gmail.com>
2023-06-04 00:07:39 +0300

    Add sitemap

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,8 +4,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
+from django.contrib.sitemaps.views import sitemap
+
+from .sitemaps import StaticViewSitemap
 
 urlpatterns = [
+    path('sitemap.xml', sitemap, {'sitemaps': {
+        'main': StaticViewSitemap,
+    }}, name="django.contrib.sitemaps.views.sitemap",),
     path('', TemplateView.as_view(template_name='core/home.html'), name='home'),
     path('admin/', admin.site.urls),
 ]
